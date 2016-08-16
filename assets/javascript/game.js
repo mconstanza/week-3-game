8793e63332941c9b76dc927fb1f6efc39940e1b3
Michael Constanza <michael.constanza@gmail.com>
2016-08-16 18:32:32 -0400

    added a working modal to display during wins/losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -522,6 +522,7 @@ winCheck = function(workingPuzzle, incorrectGuesses, puzzleSolution) {
 		wins += 1; // update the win counter
 		console.log("You win!")
 		//displayYouWin()
+		displayModal()
 
 
 	}else if (incorrectGuesses.length == 6) {
@@ -647,6 +648,33 @@ keyPress = function(event){
 	}
 }
 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+displayModal = function() {
+    modal.style.display = "block";
+    setTimeout(function(){
+    	modal.style.display = "none";}, 3000);
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 // Event Handler  //////////////////////////////////////////////////////////////////////////////////
 
